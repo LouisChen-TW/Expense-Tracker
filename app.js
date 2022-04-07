f2b66c919092eec09138844adc05216d83e06cae
LouisChen-TW <yue841206@gmail.com>
2022-04-07 11:21:00 +0800

    feat: include mogoose

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const routes = require('./routes')
 const app = express()
 const PORT = 3000
 
+require('./config/mongoose')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
