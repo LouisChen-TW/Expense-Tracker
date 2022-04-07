9dbec440a0f27e5d9e5d5a7ab9c9f69eed387444
LouisChen-TW <yue841206@gmail.com>
2022-04-07 17:11:59 +0800

    feat: add middleware authenticator at home & record router

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 const home = require('./modules/home')
 const record = require('./modules/record')
 const user = require('./modules/user')
 
 router.use('/user', user)
-router.use('/record', record)
-router.use('/', home)
+router.use('/record', authenticator, record)
+router.use('/', authenticator, home)
 
 module.exports = router
