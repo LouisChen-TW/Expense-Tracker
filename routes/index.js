8577ae487e7c7dae11097407c45803eb51a8410a
LouisChen-TW <yue841206@gmail.com>
2022-04-07 10:05:20 +0800

    feat: add user router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const record = require('./modules/record')
+const user = require('./modules/user')
 
+router.use('/user', user)
 router.use('/record', record)
 router.use('/', home)
 
