74deba8ff24da1df1325c82cbf3eb74cce20b5c2
LouisChen-TW <yue841206@gmail.com>
2022-04-08 11:59:53 +0800

    refactor: category display in home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,16 +5,20 @@ const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
-    .populate('categoryId')
+  Category.find() // for display all category in home page
     .lean()
-    .sort({ date: 'desc' })
-    .then((records) => {
-      let totalAmount = 0
-      records.forEach((record) => (totalAmount += record.amount))
-      res.render('index', { records, totalAmount })
+    .then((categories) => {
+      return Record.find({ userId })
+        .populate('categoryId')
+        .lean()
+        .sort({ date: 'desc' })
+        .then((records) => {
+          let totalAmount = 0
+          records.forEach((record) => (totalAmount += record.amount))
+          res.render('index', { records, totalAmount, categories })
+        })
+        .catch((err) => console.log(err))
     })
-    .catch((err) => console.log(err))
 })
 
 module.exports = router
