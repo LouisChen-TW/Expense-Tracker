799d95f3a66aec75e34de877c6a0027ed72dbcba
LouisChen-TW <yue841206@gmail.com>
2022-07-01 14:39:07 +0800

    add error handler

---
+++ middleware/error-handler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof Error) {
+    req.flash('error_messages', `${err.name}: ${err.message}`)
+  } else {
+    req.flash('error_messages', `${err}`)
+  }
+  next(err)
+}
