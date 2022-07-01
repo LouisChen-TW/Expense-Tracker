3ce45a939a762984141b14e3be5b5463e5cd974c
LouisChen-TW <yue841206@gmail.com>
2022-07-01 14:03:53 +0800

    add fallback route and view

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ const user = require('./modules/user')
 const auth = require('./modules/auth')
 
 router.use('/user', user)
-router.use('/record', authenticator, record)
 router.use('/auth', auth)
+router.use('/record', authenticator, record)
 router.use('/', authenticator, home)
+
+router.all('*', (req, res) => {
+  res.render('error', { site: req.originalUrl })
+})
 
 module.exports = router
